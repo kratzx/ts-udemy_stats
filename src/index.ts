a8fe5ae530acea8e92cdacacfdb1bd0ce356a5ff
Leonardo Avendaño <leonardo.avendano@phinxlab.com>
2021-11-21 17:37:33 -0300

    Finished

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
-const reader = new CsvFileReader('football.csv');
-reader.read();
-const dateOfFirstMatch = reader.data[0][0];
-console.log(dateOfFirstMatch)
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
-let manUnitedWins = 0;
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches); 
 
-console.log(`Man united won: ${manUnitedWins} matches`);
+// const summary = new Summary(
+//   new WinsAnalysis('Man United'),
+//   new ConsoleReport()
+// );
+
--- src/inheritance/CsvFileReader.ts
+++ src/inheritance/CsvFileReader.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
-export class CsvFileReader {
-  data: string[][] = [];
-
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
+  
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;   
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -13,6 +15,7 @@ export class CsvFileReader {
       (row: string): string[] => {
         return row.split(',');
       }
-    );
+    )
+    .map(this.mapRow);
   }
 }
