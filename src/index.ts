1923ac4f4ffc96cb5f66ba7b8299f6cd10d0d6c7
Leonardo Avendaño <leonardo.avendano@phinxlab.com>
2021-11-20 23:03:31 -0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello there');
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
+const reader = new CsvFileReader('football.csv');
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+console.log(dateOfFirstMatch)
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man united won: ${manUnitedWins} matches`);
